388127bc9757b1d1f36837f1db0a1a35db74a235
Stephen Dicks <stephen@starswan.com>
2020-09-17 17:56:07 +0100

    example chess.py talking to lichess.org event stream

--- src/chess.py
+++ src/chess.py
@@ -10,14 +10,18 @@ import dgt
 import berserk
 # import asyncio
 # from lichess_client import APIClient
+from twisted.internet import reactor
+from twisted.web.client import Agent, ResponseDone
+from twisted.internet.protocol import Protocol
+from twisted.internet.defer import Deferred
+from twisted.web.http_headers import Headers
+from twisted.web.iweb import UNKNOWN_LENGTH
+from twisted import version
+from twisted.python import log
+from pprint import pprint
+import sys
 
-dgtboard = dgt.DgtBoard(portname='/dev/ttyS0')
-
-TOKEN_FILE='./lichess.token'
-with open(TOKEN_FILE) as f:
-    token = f.read().strip()
-
-lichess = berserk.Client(berserk.TokenSession(token), base_url="https://lichess.dev")
+# lichess = berserk.Client(berserk.TokenSession(token), base_url="https://lichess.dev")
 # async def main():
 #     client = APIClient(token=token)
 #     response = await client.account.get_my_profile()
@@ -33,5 +37,58 @@ lichess = berserk.Client(berserk.TokenSession(token), base_url="https://lichess.
 #'and a tiny bit of OAuth2 helper header: 'Authentication: 'Bearer #{token}' but that's it.
 # asyncio would have been better, but doesn't allow override of the host name
 # Think I need to look at twisted.
-for event in lichess.board.stream_incoming_events():
-    pass
+# for event in lichess.board.stream_incoming_events():
+#     pass
+class WriteToStdout(Protocol):
+    def connectionMade(self):
+        self.onConnLost = Deferred()
+
+    def dataReceived(self, data):
+        """
+        Print out the html page received.
+        """
+        print('Got some:', data.decode('utf-8'))
+
+    def connectionLost(self, reason):
+        if not reason.check(ResponseDone):
+            reason.printTraceback()
+        else:
+            print('Response done')
+        self.onConnLost.callback(None)
+
+
+def main(reactor, url, token):
+    """
+    We create a custom UserAgent and send a GET request to a web server.
+    """
+    userAgent = 'Twisted/%s' % (version.short(),)
+    auth = 'Bearer %s' % (token)
+    agent = Agent(reactor)
+    d = agent.request(
+        bytes('GET', 'utf-8'), url, Headers({'user-agent': [userAgent], 'Authorization': [auth]}))
+    def cbResponse(response):
+        """
+        Prints out the response returned by the web server.
+        """
+        pprint(vars(response))
+        proto = WriteToStdout()
+        if response.length is not UNKNOWN_LENGTH:
+            print('The response body will consist of', response.length, 'bytes.')
+        else:
+            print('The response body length is unknown.')
+        response.deliverBody(proto)
+        return proto.onConnLost
+    d.addCallback(cbResponse)
+    d.addErrback(log.err)
+    d.addBoth(lambda ign: reactor.callWhenRunning(reactor.stop))
+    reactor.run()
+
+if __name__ == '__main__':
+    # dgtboard = dgt.DgtBoard(portname='/dev/ttyS0')
+
+    TOKEN_FILE='./lichess.org.token'
+    with open(TOKEN_FILE) as f:
+        token = f.read().strip()
+
+    main(reactor, bytes('https://lichess.org/api/stream/event', 'utf-8'), token)
+    # main(reactor, *sys.argv[1:], token)
