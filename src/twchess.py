77166f17dae080f98ba6fa629d38f7edabd3f1fc
Stephen Dicks <stephen@starswan.com>
2020-09-19 14:43:27 +0100

    dedicated protocol for receiving board changes

--- src/twchess.py
+++ src/twchess.py
@@ -39,6 +39,9 @@ import sys
 # Think I need to look at twisted.
 # for event in lichess.board.stream_incoming_events():
 #     pass
+from dgt.dgtnix import dgtnix
+
+
 class WriteToStdout(Protocol):
     def __init__(self, prefix):
         self.__prefix = prefix
@@ -59,13 +62,30 @@ class WriteToStdout(Protocol):
             print(self.__prefix + ' Response done')
         self.onConnLost.callback(None)
 
+class BoardDataReceived(Protocol):
+    def __init__(self, client):
+        self.__client = client
+
+    def dataReceived(self, data):
+        print('Got %d bytes:[%s]' % (len(data), data))
+        if data[0] == dgtnix.DGTNIX_MSG_MV_ADD:
+            file = chr(data[1])
+            rank = data[2]
+            piece = chr(data[3])
+            print("Engine received DGTNIX_MSG_MV_ADD (%s on %s%d)" % (piece, file, rank))
+            # self.__client.pieceAdded(piece, rank, file)
+        elif data[0] == dgtnix.DGTNIX_MSG_MV_REMOVE:
+            file = chr(data[1])
+            rank = data[2]
+            piece = chr(data[3])
+            print("Engine received DGTNIX_MSG_MV_REMOVE (%s on %s%d)" % (piece, file, rank))
+
 class TwistedRawSocket(abstract.FileDescriptor):
     def __init__(self, reactor, protocol, fd):
         super().__init__(reactor)
         self.__protocol = protocol
         self.__protocol.makeConnection(self)
         self.__fd = fd
-        # self.reactor.addReader(fd)
         self.startReading()
 
     def fileno(self):
@@ -81,24 +101,22 @@ class TwistedRawSocket(abstract.FileDescriptor):
         abstract.FileDescriptor.connectionLost(self, reason)
         self.__protocol.connectionLost(reason)
 
-def main(reactor):
-    dgtboard = dgt.dgt.DgtBoard(portname='/dev/ttyUSB0')
+GET_REQUEST = bytes('GET', 'utf-8')
 
-    TOKEN_FILE='./lichess.org.token'
-    with open(TOKEN_FILE) as f:
+def main(reactor, portname, tokenfile, url):
+    dgtboard = dgt.dgt.DgtBoard(portname=portname)
+
+    with open(tokenfile) as f:
         token = f.read().strip()
 
-    twisted_dgt = TwistedRawSocket(reactor, WriteToStdout('Board:'), dgtboard.pipe())
-    url = bytes('https://lichess.org/api/stream/event', 'utf-8')
+    twisted_dgt = TwistedRawSocket(reactor, BoardDataReceived(None), dgtboard.pipe())
+    url = bytes(url + '/api/stream/event', 'utf-8')
 
-    """
-    We create a custom UserAgent and send a GET request to a web server.
-    """
     userAgent = 'Twisted/%s' % (version.short(),)
     auth = 'Bearer %s' % (token)
     agent = Agent(reactor)
     d = agent.request(
-        bytes('GET', 'utf-8'), url, Headers({'user-agent': [userAgent], 'Authorization': [auth]}))
+        GET_REQUEST, url, Headers({'User-Agent': [userAgent], 'Authorization': [auth]}))
     def cbResponse(response):
         """
         Prints out the response returned by the web server.
@@ -117,5 +135,5 @@ def main(reactor):
     reactor.run()
 
 if __name__ == '__main__':
-    main(reactor)
+    main(reactor, portname='/dev/ttyUSB0', tokenfile='lichess.org.token', url='https://lichess.org')
     # main(reactor, *sys.argv[1:], token)
